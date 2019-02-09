7417c8ee40d200de85ad00bb78b71be78c4f0284
Akira46 <45726182+Akira46@users.noreply.github.com>
2019-02-09 07:41:32 +0700

    Add files via upload

---
+++ ads.js
@@ -0,0 +1,13 @@
+var player = videojs('content_video');
+
+var options = {
+  id: 'content_video',
+  adTagUrl: 'https://syndication.exdynsrv.com/splash.php?idzone=2916384'
+};
+
+player.ima(options);
+// On mobile devices, you must call initializeAdDisplayContainer as the result
+// of a user action (e.g. button click). If you do not make this call, the SDK
+// will make it for you, but not as the result of a user action. For more info
+// see our examples, all of which are set up to work on mobile devices.
+// player.ima.initializeAdDisplayContainer();
